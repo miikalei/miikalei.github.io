fdc9b89c4dd1170ea95aad85e11bc035ff1f0dae
Miika Leinonen <miika.leinonen@aalto.fi>
2025-03-16 00:12:38 +0200

    Use webworker for mlox

--- src/pages/mlox/codebox.tsx
+++ src/pages/mlox/codebox.tsx
@@ -1,28 +1,54 @@
-import { useState } from 'react'
-import { run } from './run'
+import { useEffect, useState } from 'react'
 
 export function Codebox() {
     const [code, setCode] = useState(defaultCode)
     const [output, setOutput] = useState('');
+    const [isRunning, setIsRunning] = useState(false);
+
+    const [worker, setWorker] = useState<Worker | null>(null);
+
+    useEffect(() => {
+        const worker = new Worker(new URL('run-worker.ts', import.meta.url), { type: 'module' });
+
+        console.log("Worker created:", worker);
+
+        worker.onmessage = (event) => {
+            console.log("Received event from worker:", event);
+            if (event.data.done) {
+                setIsRunning(false);
+            } else if (event.data.msg) {
+                setOutput(v => v + event.data.msg + '\n');
+            }
+        }
+
+        setWorker(worker)
+
+        return () => {
+            worker.terminate();
+        }
+    }, [])
 
     function handleRun() {
-        const runOutput = run(code);
-        setOutput(runOutput);
+        if (worker) {
+            setIsRunning(true)
+            worker.postMessage(code)
+        }
     }
 
     return <div>
         <div className="flex">
             <textarea
-                className="grow h-36 resize-none"
+                className="grow h-36 resize-none cursor-auto"
                 value={code} onChange={(e) => setCode(e.target.value)}
             ></textarea>
         </div>
-        <div className="m-4">
-            <button onClick={handleRun}>Run</button>
-            <progress>Indeterminate</progress>
+        <div className="flex my-4 items-center gap-4">
+            <button onClick={handleRun} disabled={isRunning}>Run</button>
+            {isRunning && <div className="progress-bar w-43"></div>}
         </div>
         <pre className="lowered h-36 whitespace-pre-wrap overflow-x-auto">
             {output ?? "Your code output will be displayed here"}
+            <div style={{ overflowAnchor: 'auto', height: '1px' }}></div>
         </pre>
     </div>
 }
---
+++ src/pages/mlox/run-worker.ts
@@ -0,0 +1,18 @@
+import { Run } from 'mlox/src/run'
+
+onmessage = function (event: MessageEvent) {
+    const code = event.data;
+    run(code);
+    postMessage({ done: true, msg: null });
+}
+
+function print(msg: string) {
+    postMessage({ done: false, msg })
+}
+
+function run(program: string) {
+    console.log("Running!!");
+    const runtime = new Run(print);
+    const returnCode = runtime.runProgram(program);
+    return returnCode;
+}
