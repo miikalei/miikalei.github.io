c9e3511f5bd551e3ef122aca53f21d183603f58f
Miika Leinonen <miika.leinonen@aalto.fi>
2025-03-17 22:48:41 +0200

    Add menu bars to load example programs

--- src/pages/mlox/codebox.tsx
+++ src/pages/mlox/codebox.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 export function Codebox() {
-    const [code, setCode] = useState(defaultCode)
+    const [code, setCode] = useState<string>(examplePrograms["Recursive fibonacci"]);
     const [output, setOutput] = useState('');
     const [errOutput, setErrOutput] = useState('');
     const [isRunning, setIsRunning] = useState(false);
@@ -50,7 +50,7 @@ export function Codebox() {
         setIsError(false);
         if (worker) {
             setIsRunning(true);
-            worker.postMessage(code)
+            worker.postMessage(code);
         }
     }
 
@@ -66,28 +66,49 @@ export function Codebox() {
     }
 
     return <div>
-        {errOutput}
-        <div className="flex">
-            <textarea
-                className="grow h-42 resize-none cursor-auto"
-                value={code} onChange={(e) => setCode(e.target.value)}
-            ></textarea>
-        </div>
-        <div className="flex my-4 items-center gap-4">
-            <button onClick={handleRun} disabled={isRunning}>Run</button>
-            <button onClick={handleStop} disabled={!isRunning}>Stop</button>
-            {isRunning && <div className="progress-bar w-43"></div>}
-        </div>
-        <div className={`lowered h-42 whitespace-pre-wrap overflow-x-auto ${isError ? "text-red-500" : ''}`}>
-            <div className='[overflow-anchor:none] min-h-42'>
-                {errOutput || output || "Your code output will be displayed here"}
+        <ul role="menubar">
+            <li tabIndex={0} aria-haspopup="true">
+                <u>L</u>oad example
+                <ul role="menu">
+                    {Object.entries(examplePrograms).map(([name, program], index) => {
+                        return <li tabIndex={index} onClick={() => {
+                            setCode(program);
+                            if (document.activeElement && 'blur' in document.activeElement && typeof document.activeElement.blur === 'function') {
+                                document.activeElement.blur();
+                            }
+                        }}>{name}</li>
+                    })}
+                </ul>
+            </li>
+        </ul >
+        <div className="m-0.5">
+            <p>
+                Run MLOX code anywhere, powered by modern javascript!
+                <small>Remember, this is a toy language </small>
+            </p>
+            <div className="flex">
+                <textarea
+                    className="grow h-42 resize-none cursor-auto"
+                    value={code} onChange={(e) => setCode(e.target.value)}
+                ></textarea>
             </div>
-            <div ref={anchorRef} className='[overflow-anchor:auto] [height:1px]'></div>
+            <div className="flex my-4 items-center gap-4">
+                <button onClick={handleRun} disabled={isRunning}>Run</button>
+                <button onClick={handleStop} disabled={!isRunning}>Stop</button>
+                {isRunning && <div className="progress-bar w-43"></div>}
+            </div>
+            <div className={`lowered h-42 whitespace-pre-wrap overflow-x-auto ${isError ? "text-red-500" : ''}`}>
+                <div className='[overflow-anchor:none] min-h-42'>
+                    {errOutput || output || "Your code output will be displayed here"}
+                </div>
+                <div ref={anchorRef} className='[overflow-anchor:auto] [height:1px]'></div>
+            </div>
         </div>
-    </div>
+    </div >
 }
 
-const defaultCode = `\
+const examplePrograms = {
+    "Recursive fibonacci": `\
 fun fib(n) {
   if (n <= 1) return n;
   return fib(n - 2) + fib(n - 1);
@@ -96,4 +117,26 @@ fun fib(n) {
 for (var i = 0; i < 20; i = i + 1) {
   print fib(i);
 }
+    `,
+    "Inheritance with static scoping": `\
+class A {
+          method() {
+            print "A method";
+          }
+        }
+
+        class B < A {
+          method() {
+            print "B method";
+          }
+
+          test() {
+            super.method();
+          }
+        }
+
+        class C < B {}
+
+        C().test();    
 `
+} as const;
